27742adea7d35be224cd92096dbb65b9771ca4a9
Harold Angenent <harold@trendwerk.nl>
2015-04-17 10:45:35 +0200

    Activate plugins

--- class-tp-debug.php
+++ class-tp-debug.php
@@ -14,7 +14,19 @@
 class TP_Debug {
 
 	function __construct() {
-		
+		add_action( 'admin_init', array( $this, 'activate' ) );
+	}
+
+	/**
+	 * Activate debug tools
+	 */
+	function activate() {
+		if( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG )
+			return;
+
+		activate_plugin( 'debug-bar/debug-bar.php' );
+		activate_plugin( 'debug-bar-slow-actions/debug-bar-slow-actions.php' );
+		activate_plugin( 'kint-debugger/Kint.class.php' );
 	}
 
 } new TP_Debug;
